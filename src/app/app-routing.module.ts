3d537567e64f27571ab842fba424f3f0c4c56600
adi9483g <64129845+adi9483g@users.noreply.github.com>
2020-04-22 20:09:03 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
 
-
+// set default path to '/list' and renders ListComponent
 const routes: Routes = [
   {path: '', redirectTo: '/list', pathMatch: 'full'},
   {path: 'list', component: ListComponent}
